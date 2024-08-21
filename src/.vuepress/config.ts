263f4ca13ef7f9bdc68451f8df0bbe4391f80e23
ljxpython <1030470148@qq.com>
2024-08-21 18:38:20 +0800

    修改站点base指定为blog目录

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,10 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  // 参考文档 https://vuepress.vuejs.org/zh/reference/config.html#base
+  base: "/blog/",  // 含义: 部署站点的基础路径。 如果你想让你的网站部署到一个子路径下，你将需要设置它。它的值应当总是以斜杠开始，并以斜杠结束。举例来说，如果你想将你的网站部署到 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/"。
+
+
 
   lang: "zh-CN",
   title: "测试漫游记",
